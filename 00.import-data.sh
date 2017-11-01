7c49b4da00e9d8574d0a220c723016534158c327
namgivu <namgivu@gmail.com>
2017-11-02 00:54:49 +0700

    ref.

--- 00.import-data.sh
+++ 00.import-data.sh
@@ -15,6 +15,6 @@ mkdir -p $(dirname ${IMPORT_FILE}) && \
   curl ${TEST_DATASET} -o ${IMPORT_FILE} && \
   mongoimport --db ${DB_NAME} --collection ${DB_COLLECTION} --drop --file ${IMPORT_FILE}
 
-#query via cli / aftermath check
+#query via cli / aftermath check ref. https://docs.mongodb.com/getting-started/shell/query/#query-by-a-top-level-field
 q="db.$DB_COLLECTION.find()"                            && mongo --eval "$q" ${DB_HOST}/${DB_NAME}
 q="db.$DB_COLLECTION.find( {'borough': 'Manhattan'} )"  && mongo --eval "$q" ${DB_HOST}/${DB_NAME}
