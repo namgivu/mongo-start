e453c2610dbe05be6bc8ebe07b8e29b3916a783a
namgivu <namgivu@gmail.com>
2017-11-05 18:43:03 +0700

    clean code

--- 00a.mongodb-docker-run.sh
+++ 00a.mongodb-docker-run.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-: #req. install mongodb ref. https://github.com/namgivu/deploy-util/blob/master/util/bash_util/install-mongodb.sh
-: #ref. https://hub.docker.com/_/postgres/
-
-SERVICE='mongod' #ref. https://stackoverflow.com/a/11776728/248616
-
-REGISTRY='mongo'
-IMAGE="${REGISTRY}:latest"
-PORT=27017
-CONTAINER_NAME='mongo_latest'
-DATA_FILE="$HOME/mongodb-data"
-
-sudo echo 'sudo initiated'
-sudo service ${SERVICE} stop #stop any local $SERVICE so that our docker $SERVICE will override its port
-docker rm -f ${CONTAINER_NAME}
-docker run \
-  -p ${PORT}:${PORT} \
-  -v ${DATA_FILE}:/data/db \
-  --name ${CONTAINER_NAME} ${IMAGE}
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
+version='3.2'
+version='3.4'
+${SCRIPT_HOME}/util/docker-run.sh $version
