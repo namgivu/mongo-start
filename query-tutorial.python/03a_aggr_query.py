15774e0d82b39f0a6fe579a4dfffdd38b477eacc
namgivu <namgivu@gmail.com>
2018-01-20 02:09:38 +0700

    fix bug

--- query-tutorial.python/03a_aggr_query.py
+++ query-tutorial.python/03a_aggr_query.py
@@ -15,14 +15,14 @@ p=[{ "$project": { "_id":0, "name":"$_id", "month_joined":{"$month" : "$joined"}
    { "$sort" :   { "month_joined":1  }},
 ]; qc=db.users.aggregate(p); mongo_print(qc)
 
-#total number of joins per month/year #TODO why counting via $sum always get 1?
+#total number of joins per month/year
 p=[{ "$project": { "month_joined": {"$month": "$joined"}  }},
-   { "$group":   { "_id": {"month_joined": "$month_joined", "qty": {"$sum": 1}}  }},
+   { "$group":   { "_id": {"month_joined": "$month_joined"}, "qty": {"$sum": 1}  }},
    { "$sort":    { "_id.month_joined": 1  }},
    ]; qc=db.users.aggregate(p); mongo_print(qc)
 
 p=[{ "$project": { "yyyy": {"$year": "$joined"}  }},
-   { "$group":   { "_id": {"yyyy": "$yyyy", "qty": {"$sum": 1}}  }},
+   { "$group":   { "_id": {"yyyy": "$yyyy"}, "qty": {"$sum": 1}  }},
    { "$sort":    { "_id.yyyy": 1  }},
    ]; qc=db.users.aggregate(p); mongo_print(qc)
 
