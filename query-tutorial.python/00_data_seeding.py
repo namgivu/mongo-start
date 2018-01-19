9a67d6d9fb87736c0015cf6847e993ea0c1be5d4
namgivu <namgivu@gmail.com>
2018-01-19 14:13:38 +0700

    array

--- query-tutorial.python/00_data_seeding.py
+++ query-tutorial.python/00_data_seeding.py
@@ -2,36 +2,78 @@ from common import *
 
 db.inventory.delete_many({}) #clear all first
 
+##do seeding
+
+print('BEGIN insert item qty size status')
+"""simple data ref. https://docs.mongodb.com/manual/tutorial/query-documents/#additional-query-tutorials"""
 db.inventory.insert_many([
-    {"item":   "journal",
-     "qty":    25,
+    {"item":     "journal",
+     "qty":       25,
      "size":   {"h":   14, "w":   21, "uom":   "cm"},
      "status": "A",
      },
 
-    {"item":   "notebook",
-     "qty":    50,
+    {"item":     "notebook",
+     "qty":       50,
      "size":   {"h":   8.5, "w":   11, "uom":   "in"},
      "status": "A",
+     },
+
+    {"item":     "paper",
+     "qty":       100,
+     "size":   {"h":   8.5, "w":   11, "uom":   "in"},
+     "status": "D",
+     },
+
+    {"item":     "planner",
+     "qty":       75, "size":   {"h":   22.85, "w":   30, "uom":   "cm"},
+     "status": "D",
+     },
+
+    {"item":     "postcard",
+     "qty":       45,
+     "size":   {"h":   10, "w":   15.25, "uom":   "cm"},
+     "status": "A",
+     },
+])
+print('END   insert item qty size status')
+
+print('BEGIN insert item qty tags dim_cm') #dim_cm == dimension in centimetter
+"""data has array ref. https://docs.mongodb.com/manual/tutorial/query-array-of-documents/"""
+db.inventory.insert_many([
+    {"item":   "journal",
+     "qty":    25,
+     "tags":   ["blank", "red"],
+     "dim_cm": [14, 21],
+     },
+
+    {"item":   "notebook",
+     "qty":    50,
+     "tags":   ["red", "blank"],
+     "dim_cm": [14, 21],
      },
 
     {"item":   "paper",
      "qty":    100,
-     "size":   {"h":   8.5, "w":   11, "uom":   "in"},
-     "status": "D",
+     "tags":   ["red", "blank", "plain"],
+     "dim_cm": [14, 21],
      },
 
     {"item":   "planner",
-     "qty":    75, "size":   {"h":   22.85, "w":   30, "uom":   "cm"},
-     "status": "D",
+     "qty":    75,
+     "tags":   ["blank", "red"],
+     "dim_cm": [22.85, 30],
      },
 
     {"item":   "postcard",
      "qty":    45,
-     "size":   {"h":   10, "w":   15.25, "uom":   "cm"},
-     "status": "A",
+     "tags":   ["blue"],
+     "dim_cm": [10, 15.25],
      },
-]) #do seeding
+])
+print('END   insert item qty tags dim_cm')
 
+#output
 mongo_print(query_cursor=db.inventory.find(), pretty=True)
 mongo_print(query_cursor=db.inventory.find())
+
--- query-tutorial.python/01_query.py
+++ query-tutorial.python/01_query.py
@@ -10,9 +10,12 @@ qc=db.inventory.find({"status": "D"                 }); mongo_print(qc)
 qc=db.inventory.find({"status": {"$in": ["A", "D"]} }); mongo_print(qc)
 
 #compare
-qc=db.inventory.find({"qty": 50          }); mongo_print(qc)
-qc=db.inventory.find({"qty": {"$lt": 50} }); mongo_print(qc)
+qc=db.inventory.find({"qty": 50           }); mongo_print(qc)
+qc=db.inventory.find({"qty": {"$lt": 50}  }); mongo_print(qc)
 qc=db.inventory.find({"qty": {"$lte": 50} }); mongo_print(qc)
+qc=db.inventory.find({"qty": {"$lte": 50, "$gte": 40} }); mongo_print(qc)
+
+print('query180119-1200') #a timestamp marker when we have reached many enough queries
 
 #and, or
 qc=db.inventory.find({         "status": "A"                         }); mongo_print(qc)
@@ -32,3 +35,13 @@ qc=db.inventory.find({"status": "A", "item": {"$regex": "^p"} }); mongo_print(qc
 #field.nested_field
 qc=db.inventory.find({"size.uom": "cm"}); mongo_print(qc)
 qc=db.inventory.find({"size.uom": "cm", "size.h": {"$lte": 10} }); mongo_print(qc)
+
+#data as array
+print('query180119-1400') #a timestamp marker when we have reached many enough queries
+qc = db.inventory.find({"tags":          ["red", "blank"]   }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" in the specific order
+qc = db.inventory.find({"tags": {"$all": ["red", "blank"]}  }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
+qc = db.inventory.find({"tags": {"$all": ["red"]}           }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
+qc = db.inventory.find({"tags":           "red"             }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
+
+#further on array data ref. https://docs.mongodb.com/manual/tutorial/query-arrays/#query-for-an-array-element-that-meets-multiple-criteria
+
