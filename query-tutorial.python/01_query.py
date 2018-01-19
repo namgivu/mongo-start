35319cea097aa214fb643fdab1da9702a80851ef
namgivu <namgivu@gmail.com>
2018-01-19 11:00:22 +0700

    $and filter

--- query-tutorial.python/01_query.py
+++ query-tutorial.python/01_query.py
@@ -8,3 +8,6 @@ qc=db.inventory.find({"status": {"$in": ["A", "D"]} }); mongo_print(qc)
 qc=db.inventory.find({"qty": 50          }); mongo_print(qc)
 qc=db.inventory.find({"qty": {"$lt": 50} }); mongo_print(qc)
 qc=db.inventory.find({"qty": {"$lte": 50} }); mongo_print(qc)
+
+qc=db.inventory.find({"status": "A"                     }); mongo_print(qc)
+qc=db.inventory.find({"status": "A", "qty": {"$lt": 50} }); mongo_print(qc)
