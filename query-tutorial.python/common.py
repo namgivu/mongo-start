a75340b2c555c0f1d014f055e8d1810f7415d688
namgivu <namgivu@gmail.com>
2018-01-19 14:42:15 +0700

    more util

--- query-tutorial.python/common.py
+++ query-tutorial.python/common.py
@@ -6,7 +6,7 @@ client = MongoClient('mongodb://{}:27017/'.format(DB_HOST), connect=False)
 db = client[DB_NAME]
 
 
-def mongo_print(query_cursor, pretty=False):
+def mongo_print(query_cursor, count=False, pretty=False):
     r = query_cursor; r=list(r)
     if pretty:
         from pprint import pprint
@@ -15,8 +15,14 @@ def mongo_print(query_cursor, pretty=False):
         docs=r
         for d in docs: print(d)
 
+    #print found count
+    if count:
+        print('Found {}'.format(len(r)) )
+
     #a blank line at the end as ending separator
     print()
 
+
 def exit():
+    print('Exited on purpose')
     import sys; sys.exit()
