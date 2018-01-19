2cfff4bb587e9c753f681e0410e5c8cb3485f55d
namgivu <namgivu@gmail.com>
2018-01-19 11:06:14 +0700

    $or filter

--- query-tutorial.python/01_query.py
+++ query-tutorial.python/01_query.py
@@ -1,13 +1,21 @@
 from common import *
 
+#all
 qc=db.inventory.find(); mongo_print(qc)
 
+#in
 qc=db.inventory.find({"status": "D"                 }); mongo_print(qc)
 qc=db.inventory.find({"status": {"$in": ["A", "D"]} }); mongo_print(qc)
 
+#compare
 qc=db.inventory.find({"qty": 50          }); mongo_print(qc)
 qc=db.inventory.find({"qty": {"$lt": 50} }); mongo_print(qc)
 qc=db.inventory.find({"qty": {"$lte": 50} }); mongo_print(qc)
 
-qc=db.inventory.find({"status": "A"                     }); mongo_print(qc)
-qc=db.inventory.find({"status": "A", "qty": {"$lt": 50} }); mongo_print(qc)
+#and, or
+qc=db.inventory.find({         "status": "A"                         }); mongo_print(qc)
+qc=db.inventory.find({         "status": "A",   "qty": {"$lt": 50}   }); mongo_print(qc)
+qc=db.inventory.find({"$or": [{"status": "A"}, {"qty": {"$lt": 50}}] }); mongo_print(qc)
+qc=db.inventory.find({"$or": [{"status": "A"},
+                              {"qty": {"$lt": 50}},
+                              ]                                      }); mongo_print(qc)
