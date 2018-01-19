278633a53be04378fece54104f43136b72c7876b
namgivu <namgivu@gmail.com>
2018-01-19 14:17:49 +0700

    clarify code

--- query-tutorial.python/01a_query_simple.py
+++ query-tutorial.python/01a_query_simple.py
@@ -28,20 +28,3 @@ qc=db.inventory.find({"$or": [{"status": "A"},
 #regex
 qc=db.inventory.find({"status": "A"}); mongo_print(qc)
 qc=db.inventory.find({"status": "A", "item": {"$regex": "^p"} }); mongo_print(qc)
-
-
-##further read point ref. https://docs.mongodb.com/manual/tutorial/query-documents/#additional-query-tutorials
-
-#field.nested_field
-qc=db.inventory.find({"size.uom": "cm"}); mongo_print(qc)
-qc=db.inventory.find({"size.uom": "cm", "size.h": {"$lte": 10} }); mongo_print(qc)
-
-#data as array
-print('query180119-1400') #a timestamp marker when we have reached many enough queries
-qc = db.inventory.find({"tags":          ["red", "blank"]   }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" in the specific order
-qc = db.inventory.find({"tags": {"$all": ["red", "blank"]}  }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
-qc = db.inventory.find({"tags": {"$all": ["red"]}           }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
-qc = db.inventory.find({"tags":           "red"             }); mongo_print(qc) #an array with exactly two elements, "red" and "blank" regardless to the order
-
-#further on array data ref. https://docs.mongodb.com/manual/tutorial/query-arrays/#query-for-an-array-element-that-meets-multiple-criteria
-
