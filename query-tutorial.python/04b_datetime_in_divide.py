5b3e55ab0149ab387895b44c754e501c7b24e0b2
namgivu <namgivu@gmail.com>
2018-01-21 23:13:12 +0700

    datetime in $divide

---
+++ query-tutorial.python/04b_datetime_in_divide.py
@@ -0,0 +1,24 @@
+from common import *
+
+db.sandbox.delete_many({}) #clear all first
+
+##do seeding
+ts=datetime.strptime('2018-01-01 00:00:00','%Y-%m-%d %H:%M:%S')
+tsAll=[
+  {'ts': ts},
+]
+db.sandbox.insert_many(tsAll)
+
+#output
+p=[
+  {'$project': {
+    'result': {'$divide': ['$ts', 1]}
+  }},
+
+  {'$project': {
+    '_id' : 0,
+    'ts' : 0,
+  }},
+]; qc=db.sandbox.aggregate(p); mongo_print(qc, pretty=False)
+
+# 'xx': {'$divide': ['$ts', SAMPLING_THRESHOLD]},
