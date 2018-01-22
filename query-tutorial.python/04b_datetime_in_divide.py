cbfc16e956a61625d1b4edf28841e6617ffd96e7
namgivu <namgivu@gmail.com>
2018-01-22 09:33:33 +0700

    try $millisecond

--- query-tutorial.python/04b_datetime_in_divide.py
+++ query-tutorial.python/04b_datetime_in_divide.py
@@ -12,13 +12,10 @@ db.sandbox.insert_many(tsAll)
 #output
 p=[
   {'$project': {
-    'result': {'$divide': ['$ts', 1]}
+    #'result': {'$divide': ['$ts', 1]},
+    'result' : {'$divide': [{'$millisecond':'$ts'},1] },
   }},
-
-  {'$project': {
-    '_id' : 0,
-    'ts' : 0,
-  }},
+  {'$project': {'_id' : 0,'ts' : 0} },
 ]; qc=db.sandbox.aggregate(p); mongo_print(qc, pretty=False)
 
 # 'xx': {'$divide': ['$ts', SAMPLING_THRESHOLD]},
